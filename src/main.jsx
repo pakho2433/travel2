697d9791b706db34912bc2f41828031c32d62f8b
pakho2433 <pakho2433@gmail.com>
2026-07-10 15:37:18 +0800

    Load MapLibre and open globe styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'maplibre-gl/dist/maplibre-gl.css'
 import App from './App.jsx'
 import './styles.css'
-import './googleEarth.css'
+import './mapGlobe.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
