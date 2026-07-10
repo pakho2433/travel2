7b0c504f32689ede892020b20edfc8a3bf1f3cb2
pakho2433 <pakho2433@gmail.com>
2026-07-10 12:41:20 +0800

    Load Google Earth map styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
+import './googleEarth.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
